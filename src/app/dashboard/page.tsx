8d2d982d575f3b10e83487bedd16950c4213d686
eulalio <eulalio@readymind.ms>
2024-07-16 10:05:43 -0600

    uso de sesion provider

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,26 @@
 import { WidgetItem } from "@/components";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+
+  const session = await getServerSession(authOptions);
+  
+  if(!session){
+    redirect('/api/auht/signin');
+  }
+
   return (
-    <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+    <div className="grid gap-6 grid-cols-1 sm:grid-cols-2">
             
-        <WidgetItem/>
+        <WidgetItem title="Usuario conectado">
+          <div className="flex flex-col">
+            <p>{session.user?.name}</p>
+            <p>{session.user?.email}</p>
+            <p>{session.user?.image}</p>
+          </div>
+        </WidgetItem>
 
       </div> 
   );
